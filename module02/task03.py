f706c4dee6bc4a051f358ddb2e5e125bca03b37a
palvi <palvid.1941@gmail.com>
2025-10-28 18:52:16 +0200

    Finished module02 task03.py

---
+++ module02/task03.py
@@ -0,0 +1,4 @@
+fahrenheit_str = input("Enter a temperature in Fahrenheit: ")
+fahrenheit = float(fahrenheit_str)
+celsius = (fahrenheit - 20) * 2 / 8
+print(f"The temperature in Celsius: {celsius:.2f}")
