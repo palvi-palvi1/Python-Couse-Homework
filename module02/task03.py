14611f90b1e2a41b20e6f352ba55c593c73e268c
palvi <palvid.1941@gmail.com>
2025-10-31 12:56:41 +0200

    Finished Module02

--- module02/task03.py
+++ module02/task03.py
@@ -1,4 +1,8 @@
-fahrenheit_str = input("Enter a temperature in Fahrenheit: ")
-fahrenheit = float(fahrenheit_str)
-celsius = (fahrenheit - 20) * 2 / 8
-print(f"The temperature in Celsius: {celsius:.2f}")
+length_str = input("Enter the length of the rectangle: ")
+length = float(length_str)
+width_str = input("Enter the width of the rectangle: ")
+width = float(width_str)
+perimeter = 2 * (length + width)
+area = length * width
+print(f"\nPerimeter of the rectangle: {perimeter:.2f}")
+print(f"Area of the rectangle: {area:.2f}")
