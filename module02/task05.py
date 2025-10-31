50f0bdf09be53789516c7c4e5ae3f5f460186a83
palvi <palvid.1941@gmail.com>
2025-10-31 13:14:22 +0200

    Finished Module02

--- module02/task05.py
+++ module02/task05.py
@@ -1,21 +1,20 @@
 GRAMS_PER_LOT = 13.3
 LOTS_PER_POUND = 32
 POUNDS_PER_TALENT = 20
-GRAMS_PER_KILOGRAM = 1000
 
 talents = float(input("Enter talents: \n"))
 pounds = float(input("Enter pounds: \n"))
 lots = float(input("Enter lots: \n"))
 
 
-total_lots = (talents * POUNDS_PER_TALENT * LOTS_PER_POUND) + \
-             (pounds * LOTS_PER_POUND) + lots
+grams_from_talents = talents * POUNDS_PER_TALENT * LOTS_PER_POUND * GRAMS_PER_LOT
+grams_from_pounds = pounds * LOTS_PER_POUND * GRAMS_PER_LOT
+grams_from_lots = lots * GRAMS_PER_LOT
 
-total_grams = total_lots * GRAMS_PER_LOT
+total_grams = grams_from_talents + grams_from_pounds + grams_from_lots
 
-kilograms = int(total_grams // GRAMS_PER_KILOGRAM)
-remaining_grams = total_grams % GRAMS_PER_KILOGRAM
+kilograms = int(total_grams // 1000)
+grams = total_grams % 1000
 
-
-print("\nThe weight in modern units:")
-print(f"{kilograms} kilograms and {remaining_grams:.2f} grams.")
+print("\nThe weight in modern units: ")
+print(f"{kilograms} kilograms and {grams:.2f} grams.")
