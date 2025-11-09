57c102212241b352b84616ee8f4445da56666ff0
palvi <palvid.1941@gmail.com>
2025-11-09 17:48:30 +0200

    Finnish Module04

--- module04/task01.py
+++ module04/task01.py
@@ -1,10 +1,6 @@
 number = 1
-count = 0
 while number <= 1000:
     if number % 3 == 0:
         print(number)
-        number += 1
     number += 1
-
-    print(f"total numbers divisible by 3: {count}")
 
