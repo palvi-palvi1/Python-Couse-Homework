2e7f7f21dccc88b9f13362b0b7bd61aeb3ad8d9d
palvi <palvid.1941@gmail.com>
2025-11-09 17:05:12 +0200

    Finnish Module04

--- module04/task02.py
+++ module04/task02.py
@@ -1,6 +1,6 @@
 inches = float(input("enter inches (enter negative value to stop): "))
 
 while inches >= 0:
-    centimeters = inches / 2.54
+    centimeters = inches * 2.54
     print(f"{inches} inches is {centimeters} centimeters")
     inches = float(input("enter inches (enter negative value to stop): "))
