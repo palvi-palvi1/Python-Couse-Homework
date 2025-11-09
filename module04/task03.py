1916dfd0a9fe0f773449e98cadb1bd667787b67f
palvi <palvid.1941@gmail.com>
2025-11-09 17:57:01 +0200

    Finnish Module04

--- module04/task03.py
+++ module04/task03.py
@@ -1,12 +1,21 @@
 numbers = []
-user_input = input("enter a number (blank to stop): ")
-while user_input != "":
-    numbers.append(float(user_input))
-    user_input = input("enter a number (blank to stop): ")
-    if numbers:
-        print(f"smallest number: {min(numbers)}")
-        print(f"largest number: {max(numbers)}")
+while True:
+    user_input = input("Enter a number (leave blank to stop): ")
+
+    if user_input == "":
+        break
+    try:
+        num = float(user_input)
+        numbers.append(num)
+    except ValueError:
+        print("Please enter a valid number.")
+
+if numbers:
+    smallest = min(numbers)
+    largest = max(numbers)
+    print(f"\nSmallest number: {smallest}")
+    print(f"Largest number: {largest}")
 
 
-    else:
-        print("no numbers were entered.")
+else:
+    print("\nNo numbers were entered.")
