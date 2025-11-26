02141d8bf786732959479915d15881515bc56e5e
palvi <palvid.1941@gmail.com>
2025-11-26 10:41:13 +0200

    Finnish Module04

--- module04/task03.py
+++ module04/task03.py
@@ -1,21 +1,12 @@
-numbers = []
-while True:
-    user_input = input("Enter a number (leave blank to stop): ")
-
-    if user_input == "":
-        break
-    try:
-        num = float(user_input)
-        numbers.append(num)
-    except ValueError:
-        print("Please enter a valid number.")
-
-if numbers:
-    smallest = min(numbers)
-    largest = max(numbers)
-    print(f"\nSmallest number: {smallest}")
-    print(f"Largest number: {largest}")
-
-
-else:
-    print("\nNo numbers were entered.")
+number_input = input("enter a number (blank to stop): ")
+smallest_number = None
+largest_number = None
+while number_input != "":
+    number = float(number_input)
+    if smallest_number is None or number < smallest_number:
+        smallest_number = number
+    if largest_number is None or number > largest_number:
+        largest_number = number
+    number_input = input("enter a number (blank to stop): ")
+print("smallest number entered:", smallest_number)
+print("largest number entered:", largest_number)
