af55b5ebd0f91165e1191e678f11a1a541f495d7
palvi <palvid.1941@gmail.com>
2025-11-26 10:35:15 +0200

    Finnish Module02

--- module02/task04.py
+++ module02/task04.py
@@ -8,7 +8,7 @@ product_result = num1 * num2 * num3
 
 average_result = sum_result / 3
 
-print("\n--- Results ---")
+print("\nResults")
 print(f"Sum: {sum_result}")
 print(f"Product: {product_result}")
 print(f"Average: {average_result:.2f}") # Format average to two decimals
