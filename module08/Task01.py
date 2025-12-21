197f5d9fd1e284629ffdf3fd268d9bd78c77cc1e
Palvi <palvid.1941@gmail.com>
2025-12-21 13:12:55 +0200

    Finnish Module07

--- module08/Task01.py
+++ module08/Task01.py
@@ -4,11 +4,8 @@ class Car:
         self.maximum_speed = maximum_speed
         self.current_speed = 0
         self.travelled_distance = 0
-def main():
-    new_car = Car("ABC-123", 142)
-    print("New Car Information:")
-    print(f"Registration Number: {new_car.registration_number}")
-    print(f"Maximum Speed: {new_car.maximum_speed} km/h")
-    print(f"Current Speed: {new_car.current_speed} km/h")
-    print(f"Travelled Distance: {new_car.travelled_distance} km")
-main()
+car = Car("ABC-123", 142)
+print("Registration Number:", car.registration_number)
+print("Maximum Speed: ", car.maximum_speed, "km/h")
+print("Current Speed: ", car.current_speed, "km/h")
+print("Travel Distance: ", car.travelled_distance, "km")
--- module08/Task02.py
+++ module08/Task02.py
@@ -10,10 +10,14 @@ class Car:
             self.current_speed = self.maximum_speed
         if self.current_speed < 0:
             self.current_speed = 0
+
 car = Car("ABC-123", 142)
 car.accelerate(30)
 car.accelerate(70)
 car.accelerate(50)
+
 print("Current speed: ", car.current_speed, "km/h")
+
 car.accelerate(-200)
+
 print("Final speed: ", car.current_speed, "km/h")
