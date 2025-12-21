4908ae0e9117ace164298d66e0573df2787fc9f4
palvi-palvi1 <palvid.1941@gmail.com>
2025-12-21 15:13:51 +0200

    Fix formatting issues in Task03.py

--- module08/Task03.py
+++ module08/Task03.py
@@ -4,14 +4,12 @@ class Car:
         self.maximum_speed = maximum_speed
         self.current_speed = 0
         self.travelled_distance = 0
-
     def accelerate(self, change):
         self.current_speed += change
         if self.current_speed > self.maximum_speed:
             self.current_speed = self.maximum_speed
         if self.current_speed < 0:
             self.current_speed = 0
-
     def drive(self, hours):
         self.travelled_distance += self.current_speed * hours
 car = Car("ABC-123", 142)
