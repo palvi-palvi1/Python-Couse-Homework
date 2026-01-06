86417dc0ae4d412aa7b072013f50ad8a7c13b1c2
palvi-palvi1 <palvid.1941@gmail.com>
2026-01-06 17:12:40 +0200

    Replace car instance with new_car and update prints

--- module08/Task01.py
+++ module08/Task01.py
@@ -4,8 +4,9 @@ class Car:
         self.maximum_speed = maximum_speed
         self.current_speed = 0
         self.travelled_distance = 0
-car = Car("ABC-123", 142)
-print("Registration Number:", car.registration_number)
-print("Maximum Speed: ", car.maximum_speed, "km/h")
-print("Current Speed: ", car.current_speed, "km/h")
-print("Travel Distance: ", car.travelled_distance, "km")
+new_car = Car("ABC-123", "142")
+print(new_car.registration_number)
+print(new_car.maximum_speed)
+print(new_car.current_speed)
+print(new_car.travelled_distance)
+
