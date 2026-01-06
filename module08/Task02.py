0d570e3ef28b144a99e5e2b64b64fbf9d529dd9f
palvi-palvi1 <palvid.1941@gmail.com>
2026-01-06 17:37:31 +0200

    Module08 completed

--- module08/Task02.py
+++ module08/Task02.py
@@ -1,19 +1,20 @@
 class Car:
-    def __init__(self, registration_number, maximum_speed):
-        self.registration_number = registration_number
+    def __init__(self , maximum_speed):
         self.maximum_speed = maximum_speed
         self.current_speed = 0
-        self.travelled_distance = 0
-    def accelerate(self, change):
-        self.current_speed += change
+    def accelerate(self, change_of_speed):
+        self.current_speed += change_of_speed
+
         if self.current_speed > self.maximum_speed:
             self.current_speed = self.maximum_speed
         if self.current_speed < 0:
             self.current_speed = 0
-car = Car("ABC-123", 142)
+
+car = Car(150)
 car.accelerate(30)
 car.accelerate(70)
 car.accelerate(50)
-print("Current speed: ", car.current_speed, "km/h")
+
+print(f"crrent speed: {car.current_speed}")
 car.accelerate(-200)
-print("Final speed: ", car.current_speed, "km/h")
+print(f"final speed: {car.current_speed}")
