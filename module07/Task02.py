233fa958415b9698f76eb10c0dfdbd0766061f1b
palvi-palvi1 <palvid.1941@gmail.com>
2026-01-08 12:05:47 +0200

    completed all

--- module07/Task02.py
+++ module07/Task02.py
@@ -1,15 +1,12 @@
-def convert_to_liters(gallons):
-    liters = gallons * 3.78541
-    return liters
-def main():
-    print("gallons to liters converter or enter negative number to quit")
+def convertor(value):
+    liter = 3.78541
+    return value * liter
 
-    while True:
-        user_input = float(input("\nEnter volume in gallons: "))
-
-        if user_input < 0:
-            print("Exiting program. Goodbye!")
-            break
-        result = convert_to_liters(user_input)
-        print(f"{user_input} gallons is {result:.2f} liters.")
-main()
+while True:
+    quantity_Gasoline = int(input("enter the quantity of gasoline:"))
+    if quantity_Gasoline < 0:
+        print("You entered negative number.")
+        break
+    else:
+        result = convertor(quantity_Gasoline)
+        print(f"{quantity_Gasoline} gallons is equal to {result} liters.")
